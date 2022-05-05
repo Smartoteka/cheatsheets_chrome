bdb2e34d6f96342be25f40e06d9ba2a646e98599
Mikhail Tatarkov <tmu87@mail.ru>
2022-05-05 09:43:56 +0300

    Fix extension reopen (duplicates)

--- src/background/background.js
+++ src/background/background.js
@@ -1,73 +1,82 @@
 import storage from '../utils/storage'
-import { getActiveTab } from '../src_jq/common/commonFunctions'
+import { getActiveTab, queryTabs } from '../src_jq/common/commonFunctions'
 
-async function getOrCreatePopup(activeTab, url, width, height, isAddMode) {
+async function getOrCreatePopup(windowName, activeTab, url, width, height, isAddMode) {
+  const windowStorageId = windowName + chrome.runtime.id
+
   let createWindowCallback = async (openedWindow) => {
     let value = {}
-    value[chrome.runtime.id] = openedWindow.id
+    value[windowStorageId] = openedWindow.id
+    console.log('save ' + openedWindow.id)
     await storage.set(value)
+
+    chrome.windows.onRemoved.addListener(
+      async (windowId) => {
+        let currentPopupId = await storage.get(windowStorageId)
+        console.log('currentPopupId  ' + currentPopupId)
+        if (windowId === currentPopupId) {
+          currentPopupId = null
+        }
+      },
+      { windowTypes: ['popup'] },
+    )
   }
 
   let [displayInfo] = await chrome.system.display.getInfo()
 
-  let create = (top) => chrome.windows.create(
-    {
-      url: chrome.runtime.getURL(url) + '?cmd=' + (isAddMode ? 'to-add' : 'clear'),
-      type: 'popup',
-      focused: true,
-      height: height,
-      width: width,
-      top: top,
-      left: displayInfo.bounds.width - width,
-      // alwaysOnTop: true,
-    },
-    createWindowCallback,
-  )
+  let create = (top) => {
+    console.log('Create popup!')
 
-  let currentPopupId = await storage.get(chrome.runtime.id)
+    chrome.windows.create(
+      {
+        url: chrome.runtime.getURL(url) + '?cmd=' + (isAddMode ? 'to-add' : 'clear'),
+        type: 'popup',
+        focused: true,
+        height: height,
+        width: width,
+        top: top,
+        left: displayInfo.bounds.width - width,
+        // alwaysOnTop: true,
+      },
+      createWindowCallback,
+    )
+  }
+
+  let currentPopupId = await storage.get(windowStorageId)
 
   let open = async (top) => {
     if (currentPopupId) {
-      chrome.windows.update(currentPopupId,
-        { focused: true },
-        (openWindow) => {
-          if (openWindow) {
-            getActiveTab().then((tab) => {
-            //   chrome.tabs.update(tab.id, { url: chrome.runtime.getURL(url) + '?cmd=' + (isAddMode ? 'to-add' : 'clear') })
-              chrome.tabs.sendMessage(tab.id, (isAddMode ? 'to-add' : 'clear'), function (response) {
-                console.log(response)
-              })
-            })
-            return
-          }
+      console.log('update popup!')
+      try {
+        await chrome.windows.update(currentPopupId,
+          { focused: true })
 
-          create(top)
+        queryTabs(currentPopupId).then((tab) => {
+          if (tab && tab.url.indexOf(chrome.runtime.id) !== -1) {
+            chrome.tabs.sendMessage(tab.id, (isAddMode ? 'to-add' : 'clear'), function (response) {
+              console.log(response)
+            })
+          } else {
+            console.log(tab)
+            create(top)
+          }
         })
+      } catch (error) {
+        console.log('Not open window')
+        create(top)
+      }
     } else {
+      console.log('Not currentPopupId')
       create(top)
     }
   }
 
-  let value = {}
-  value.windowId = activeTab?.windowId
-  await storage.set(value)
-
-  await open(displayInfo.bounds.height - height)
+  await open(100)
 }
-
-chrome.windows.onRemoved.addListener(
-  async (windowId) => {
-    let currentPopupId = await storage.get(chrome.runtime.id)
-    if (windowId === currentPopupId) {
-      currentPopupId = null
-    }
-  },
-  { windowTypes: ['popup'] },
-)
 
 let popupUrl = 'popup/popup.html'
 async function openPopup(tab, isAddMode) {
-  await getOrCreatePopup(tab, popupUrl, 500, tab ? tab.height : 1000, isAddMode)
+  await getOrCreatePopup('popup', tab, popupUrl, 500, tab ? tab.height : 1000, isAddMode)
 }
 
 chrome.commands.onCommand.addListener(async (command) => {
@@ -75,12 +84,23 @@ chrome.commands.onCommand.addListener(async (command) => {
   switch (command) {
     case 'open-search': {
       getActiveTab()
-        .then(activeTab => openPopup(activeTab, false))
+        .then(activeTab => {
+          storage.set({ windowId: activeTab.windowId })
+          openPopup(activeTab, false)
+        })
+      break
+    }
+    case 'open-search-on-page': {
+      getActiveTab()
+        .then(tab => getOrCreatePopup('search', tab, 'search/search.html', 500, 400, false))
       break
     }
     case 'add-tab-to-session': {
       getActiveTab()
-        .then(activeTab => openPopup(activeTab, true))
+        .then(activeTab => {
+          storage.set({ windowId: activeTab.windowId })
+          openPopup(activeTab, true)
+        })
       break
     }
     default: {
